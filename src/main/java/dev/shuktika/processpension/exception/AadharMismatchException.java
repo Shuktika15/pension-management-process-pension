8c4018e6e7392f88340a8e072a65ccec015a1146
Shuktika Mahanty <shuktika.mahanty@gmail.com>
2022-07-28 22:10:45 +0530

    Update error handling logic

--- src/main/java/dev/shuktika/processpension/exception/AadharMismatchException.java
+++ src/main/java/dev/shuktika/processpension/exception/AadharMismatchException.java
@@ -1,5 +1,9 @@
 package dev.shuktika.processpension.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class AadharMismatchException extends RuntimeException {
     public AadharMismatchException(String msg) {
         super(msg);
